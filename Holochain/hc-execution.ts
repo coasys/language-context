b8dde22329023587148b70763cf950bf4f1980c2
jdeepee <joshuadparkin@gmail.com>
2021-05-04 18:46:13 +0100

    add string escape to process calls

--- Holochain/hc-execution.ts
+++ Holochain/hc-execution.ts
@@ -4,16 +4,20 @@ import fs from "fs";
 //This may be needed as bootstrap url on sandboxes to help them find eachother
 const bootStrapUrl = "https://bootstrap-staging.holo.host"
 
+function escapeShellArg (arg) {
+    return arg.replace(" ", "\\\ ");
+}
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 export function createSandbox(hcPath, sbPath) {
-    return child_process.execSync(`${hcPath} sandbox create --root ${sbPath} network --bootstrap ${bootStrapUrl} quic`).toString();
+    return child_process.execSync(`${escapeShellArg(hcPath)} sandbox create --root ${escapeShellArg(sbPath)} network --bootstrap ${bootStrapUrl} quic`).toString();
 }
 
 export function readSandboxes(hcPath) {
-    let sb = child_process.execSync(`${hcPath} sandbox list`).toString();
+    let sb = child_process.execSync(`${escapeShellArg(hcPath)} sandbox list`).toString();
     let lines = sb.split(/\r\n|\r|\n/);
     if (lines.length == 4) {
         return []
@@ -27,27 +31,27 @@ export function readSandboxes(hcPath) {
 }
 
 export function stopProcesses(sbPath, hcProcess, lairProcess) {
-    fs.unlinkSync(`${sbPath}/keystore/pid`)
+    fs.unlinkSync(`${escapeShellArg(sbPath)}/keystore/pid`)
     hcProcess.kill("SIGINT");
     lairProcess.kill("SIGINT");
 }
 
 export function unpackDna(hcPath, dnaPath) {
-    return child_process.execSync(`${hcPath} dna unpack ${dnaPath}`).toString();
+    return child_process.execSync(`${escapeShellArg(hcPath)} dna unpack ${escapeShellArg(dnaPath)}`).toString();
 }
 
 export function packDna(hcPath, workdirPath) {
-    return child_process.execSync(`${hcPath} dna pack ${workdirPath}`).toString();
+    return child_process.execSync(`${escapeShellArg(hcPath)} dna pack ${escapeShellArg(workdirPath)}`).toString();
 }
 
 export async function runSandbox(lairPath, hcPath, holochainPath, sbPath, adminPort) {
-    let lairProcess = child_process.spawn(`${lairPath}`, [], {
+    let lairProcess = child_process.spawn(`${escapeShellArg(lairPath)}`, [], {
         stdio: "inherit",
-        env: { ...process.env, LAIR_DIR: `${sbPath}/keystore` },
+        env: { ...process.env, LAIR_DIR: `${escapeShellArg(sbPath)}/keystore` },
     });
     await sleep(500);
     
-    let hcProcess = child_process.spawn(`${hcPath}`, ["sandbox", "-f", adminPort, "--holochain-path", `${holochainPath}`, "run", "-e", sbPath],
+    let hcProcess = child_process.spawn(`${escapeShellArg(hcPath)}`, ["sandbox", "-f", adminPort, "--holochain-path", `${escapeShellArg(holochainPath)}`, "run", "-e", sbPath],
         {
             stdio: "inherit",
             env: {
@@ -57,7 +61,7 @@ export async function runSandbox(lairPath, hcPath, holochainPath, sbPath, adminP
         }
     );
     process.on("SIGINT", function () {
-        fs.unlinkSync(`${sbPath}/keystore/pid`)
+        fs.unlinkSync(`${escapeShellArg(sbPath)}/keystore/pid`)
         hcProcess.kill("SIGINT");
         lairProcess.kill("SIGINT");
         process.exit();
@@ -68,5 +72,5 @@ export async function runSandbox(lairPath, hcPath, holochainPath, sbPath, adminP
 }
 
 export function cleanSandboxes(hcPath) {
-    return child_process.execSync(`${hcPath} sandbox clean`).toString();
+    return child_process.execSync(`${escapeShellArg(hcPath)} sandbox clean`).toString();
 }
